9d1e7e808f2211e0ed755f563eaca16c41ae8c1d
Jussi Viinikainen <jviinika@uic.edu>
2018-04-23 11:37:57 -0500

    Added saving figures to file

--- plotting/dijetPlotter.C
+++ plotting/dijetPlotter.C
@@ -46,7 +46,7 @@ TH1D* findHistogram(TFile *inputFile, const char *name, int lowBinIndex, int hig
  *  Arguments:
  *   TString inputFileName = File, from which the histograms are plotter
  */
-void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
+void dijetPlotter(TString inputFileName = "data/dijetSpectraTestPbPb.root"){
   
   // Write the file name to console
   cout << "Plotting histograms from " << inputFileName.Data() << endl;
@@ -54,16 +54,19 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
   // =========== Configuration ============
   
   // Choose which figure sets to draw
-  bool drawEventInformation = false;
-  bool drawDijetHistograms = false;
-  bool drawJetPtHistograms = false;
-  bool drawJetPhiHistograms = false;
-  bool drawJetEtaHistograms = false;
+  bool drawEventInformation = true;
+  bool drawDijetHistograms = true;
+  bool drawJetPtHistograms = true;
+  bool drawJetPhiHistograms = true;
+  bool drawJetEtaHistograms = true;
   bool drawTwoDimensionalHistograms = true;
   
+  // Choose if you want to write the figures to pdf file
+  bool saveFigures = true;
+  
   // Define binning to project out from THnSparses
-  const int nCentralityBins = 4;
-  double centralityBinBorders[nCentralityBins+1] = {0,10,30,50,100};
+  const int nCentralityBins = 1;
+  double centralityBinBorders[nCentralityBins+1] = {-0.5,100};
   int centralityBinIndices[nCentralityBins+1] = {0};
   
   // Choose which centrality bins to draw
@@ -155,36 +158,65 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
   // Read the system name from the card
   TString collisionSystem = card->GetDataType();
   TString systemAndEnergy = Form("%s 5.02 TeV",collisionSystem.Data());
+  TString compactSystemAndEnergy = systemAndEnergy;
+  compactSystemAndEnergy.ReplaceAll(" ","");
+  compactSystemAndEnergy.ReplaceAll(".","v");
   TString centralityString;
+  TString compactCentralityString;
   
   // Draw event information histograms
   if(drawEventInformation){
+    
+    // === Vertex z-position ===
     drawer->DrawHistogram(hVertexZ,"v_{z} (cm)","#frac{dN}{dv_{z}}  (1/cm)"," ");
     legend = new TLegend(0.62,0.75,0.82,0.9);
     legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
     legend->AddEntry((TObject*) 0, systemAndEnergy.Data(), "");
     legend->Draw();
     
-    drawer->DrawHistogram(hEvents,"Event class","Number of events", " ");
+    // Save the figures to file
+    if(saveFigures){
+      TString figName = Form("figures/vz_%s",compactSystemAndEnergy.Data());
+      gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+    }
+    
+    // === Event cuts ===
+    drawer->DrawHistogram(hEvents,"Event cuts","Number of events", " ");
     legend = new TLegend(0.17,0.22,0.37,0.37);
     legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
     legend->AddEntry((TObject*) 0, systemAndEnergy.Data(), "");
     legend->Draw();
     
+    // Save the figures to file
+    if(saveFigures){
+      TString figName = Form("figures/eventCuts_%s",compactSystemAndEnergy.Data());
+      gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+    }
+    
+    // === Centrality ===
     drawer->DrawHistogram(hCentrality,"Centrality percentile","N"," ");
     legend = new TLegend(0.62,0.75,0.82,0.9);
     legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
     legend->AddEntry((TObject*) 0, systemAndEnergy.Data(), "");
     legend->Draw();
+    
+    // Save the figures to file
+    if(saveFigures){
+      TString figName = Form("figures/centrality_%s",compactSystemAndEnergy.Data());
+      gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+    }
   }
   
   for(int iCentrality = firstDrawnCentralityBin; iCentrality <= lastDrawnCentralityBin; iCentrality++){
     
     drawer->SetRightMargin(0.06);
     centralityString = Form("Cent: %.0f-%.0f%%",centralityBinBorders[iCentrality],centralityBinBorders[iCentrality+1]);
+    compactCentralityString = Form("_C=%.0f-%.0f",centralityBinBorders[iCentrality],centralityBinBorders[iCentrality+1]);
     
     // Draw dijet histograms
     if(drawDijetHistograms){
+      
+      // === Dijet DeltaPhi ===
       drawer->DrawHistogram(hDijetDphi[iCentrality],"#Delta#varphi","#frac{dN}{d#Delta#varphi}"," ");
       legend = new TLegend(0.17,0.75,0.37,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
@@ -192,16 +224,33 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hDijetDphi[iCentrality],centralityString.Data(),"");
       legend->Draw();
       
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/deltaPhi_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
+      
+      // === Dijet asymmetry ===
       drawer->DrawHistogram(hDijetAsymmetry[iCentrality],"A_{jj}","#frac{dN}{dA_{jj}}"," ");
       legend = new TLegend(0.62,0.75,0.82,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
       legend->AddEntry((TObject*) 0, systemAndEnergy.Data(), "");
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hDijetAsymmetry[iCentrality],centralityString.Data(),"");
       legend->Draw();
+      
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/asymmetry_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
     }
     
     // Draw pT histograms for jets
     if(drawJetPtHistograms){
+      
+      // === Leading jet pT ===
       drawer->DrawHistogram(hLeadingJetPt[iCentrality],"Leading jet p_{T}  (GeV)","#frac{dN}{dp_{T}}  (1/GeV)"," ");
       legend = new TLegend(0.62,0.75,0.82,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
@@ -209,6 +258,14 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hLeadingJetPt[iCentrality],centralityString.Data(),"");
       legend->Draw();
       
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/leadingJetPt_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
+      
+      // === Subleading jet pT ===
       drawer->DrawHistogram(hSubleadingJetPt[iCentrality],"Subleading jet p_{T}  (GeV)","#frac{dN}{dp_{T}}  (1/GeV)"," ");
       legend = new TLegend(0.62,0.75,0.82,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
@@ -216,16 +273,33 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hSubleadingJetPt[iCentrality],centralityString.Data(),"");
       legend->Draw();
       
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/subleadingJetPt_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
+      
+      // === Any jet pT ===
       drawer->DrawHistogram(hAnyJetPt[iCentrality],"Any jet p_{T}  (GeV)","#frac{dN}{dp_{T}}  (1/GeV)"," ");
       legend = new TLegend(0.62,0.75,0.82,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
       legend->AddEntry((TObject*) 0, systemAndEnergy.Data(), "");
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hAnyJetPt[iCentrality],centralityString.Data(),"");
       legend->Draw();
+      
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/anyJetPt_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
     }
     
     // Draw phi histograms for jets
     if(drawJetPhiHistograms){
+      
+      // === Leading jet phi ===
       drawer->DrawHistogram(hLeadingJetPhi[iCentrality],"Leading jet #varphi","#frac{dN}{d#varphi}"," ");
       legend = new TLegend(0.62,0.75,0.82,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
@@ -233,6 +307,14 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hLeadingJetPhi[iCentrality],centralityString.Data(),"");
       legend->Draw();
       
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/leadingJetPhi_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
+      
+      // === Subleading jet phi ===
       drawer->DrawHistogram(hSubleadingJetPhi[iCentrality],"Subleading jet #varphi","#frac{dN}{d#varphi}"," ");
       legend = new TLegend(0.62,0.75,0.82,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
@@ -240,16 +322,33 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hSubleadingJetPhi[iCentrality],centralityString.Data(),"");
       legend->Draw();
       
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/subleadingJetPhi_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
+      
+      // === Any jet phi ===
       drawer->DrawHistogram(hAnyJetPhi[iCentrality],"Any jet #varphi","#frac{dN}{d#varphi}"," ");
       legend = new TLegend(0.62,0.75,0.82,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
       legend->AddEntry((TObject*) 0, systemAndEnergy.Data(), "");
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hAnyJetPhi[iCentrality],centralityString.Data(),"");
       legend->Draw();
+      
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/anyJetPhi_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
     }
     
     // Draw eta histograms for jets
     if(drawJetEtaHistograms){
+      
+      // === Leading jet eta ===
       drawer->DrawHistogram(hLeadingJetEta[iCentrality],"Leading jet #eta","#frac{dN}{d#eta}"," ");
       legend = new TLegend(0.62,0.20,0.82,0.35);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
@@ -257,6 +356,14 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hLeadingJetEta[iCentrality],centralityString.Data(),"");
       legend->Draw();
       
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/leadingJetEta_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
+      
+      // === Subleading jet eta ===
       drawer->DrawHistogram(hSubleadingJetEta[iCentrality],"Subleading jet #eta","#frac{dN}{d#eta}"," ");
       legend = new TLegend(0.62,0.20,0.82,0.35);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
@@ -264,18 +371,35 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hSubleadingJetEta[iCentrality],centralityString.Data(),"");
       legend->Draw();
       
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/subleadingJetEta_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
+      
+      // === Any jet eta ===
       drawer->DrawHistogram(hAnyJetEta[iCentrality],"Any jet #eta","#frac{dN}{d#eta}"," ");
       legend = new TLegend(0.62,0.20,0.82,0.35);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
       legend->AddEntry((TObject*) 0, systemAndEnergy.Data(), "");
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hAnyJetEta[iCentrality],centralityString.Data(),"");
       legend->Draw();
+      
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/anyJetEta_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
     }
     
     // Draw 2D histograms
     drawer->SetRightMargin(0.1);
     
     if(drawTwoDimensionalHistograms){
+      
+      // === Dijet asymmetry vs. DeltaPhi ===
       drawer->DrawHistogram(hDijetAsymmetryVsDphi[iCentrality],"#Delta#varphi","A_{jj}"," ","colz");
       legend = new TLegend(0.17,0.75,0.37,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
@@ -283,12 +407,27 @@ void dijetPlotter(TString inputFileName = "../dijetSpectraTest_1.root"){
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hDijetAsymmetryVsDphi[iCentrality],centralityString.Data(),"");
       legend->Draw();
       
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/dijetAsymmetryVsDeltaPhi_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
+      
+      // === Leading jet pT vs. subleading jet pT ===
       drawer->DrawHistogram(hDijetLeadingVsSubleadingPt[iCentrality],"Leading jet p_{T}","Subleading jet p_{T}"," ","colz");
       legend = new TLegend(0.17,0.75,0.37,0.9);
       legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
       legend->AddEntry((TObject*) 0, systemAndEnergy.Data(), "");
       if(collisionSystem.Contains("PbPb")) legend->AddEntry(hDijetLeadingVsSubleadingPt[iCentrality],centralityString.Data(),"");
       legend->Draw();
+      
+      // Save the figures to file
+      if(saveFigures){
+        TString figName = Form("figures/leadingJetPtVsSubleadingJetPt_%s",compactSystemAndEnergy.Data());
+        if(collisionSystem.Contains("PbPb")) figName.Append(compactCentralityString);
+        gPad->GetCanvas()->SaveAs(Form("%s.pdf",figName.Data()));
+      }
     }
   }
 }
