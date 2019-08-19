09452e84bea8e78bb48b315f18914927196ea2ec
Jussi Viinikainen <jviinika@uic.edu>
2019-08-19 18:55:06 -0500

    Print also pp and pT integrated uncertainties

--- plotting/printRelativeUncertainty.C
+++ plotting/printRelativeUncertainty.C
@@ -11,12 +11,17 @@ void printRelativeUncertainty(){
   // ========================= Configuration ==========================
   // ==================================================================
   
-  TString dataFileName = "data/dijetPbPb_pfCsJets_xjBins_wtaAxis_noUncOrInc_improvisedMixing_allCorrections_processed_2019-07-05.root"; // Compare also with uncorrected data
-  TString uncertaintyFileName = "systematicTest.root";
+  TString dataFileName = "data/dijetPbPb2018_highForest_akFlowPuCs4PfJets_5eveMix_xjBins_allCorrections_modifiedSeagull_wtaAxis_JECv4_processed_2019-08-13_fiveJobsMissing.root"; // Compare also with uncorrected data
+  // data/dijetPbPb_pfCsJets_xjBins_wtaAxis_noUncOrInc_improvisedMixing_allCorrections_processed_2019-07-05.root
+  TString ppFileName = "data/ppData2017_highForest_pfJets_20eventsMixed_xjBins_JECv2_averagePeakMixing_wtaAxis_allCorrections_processed_2019-08-13.root";
+  TString uncertaintyFileName = "systematicTestPbPb.root";
+  TString ppUncertainryFileName = "systematicTestPp.root";
   
   // Open the input files
   TFile *dataFile = TFile::Open(dataFileName);
   TFile *uncertaintyFile = TFile::Open(uncertaintyFileName);
+  TFile *ppFile = TFile::Open(ppFileName);
+  TFile *ppUncertainryFile = TFile::Open(ppUncertainryFileName);
   
   // Make readers to read the files
   DijetHistogramManager *dataManager = new DijetHistogramManager(dataFile);
@@ -24,74 +29,105 @@ void printRelativeUncertainty(){
   dataManager->SetLoadTrackSubleadingJetCorrelationsPtWeighted(true);
   dataManager->LoadProcessedHistograms();
   
+  DijetHistogramManager *ppManager = new DijetHistogramManager(ppFile);
+  ppManager->SetLoadTrackLeadingJetCorrelationsPtWeighted(true);
+  ppManager->SetLoadTrackSubleadingJetCorrelationsPtWeighted(true);
+  ppManager->LoadProcessedHistograms();
+  
   JffCorrector *uncertaintyManager = new JffCorrector();
   uncertaintyManager->ReadSystematicFile(uncertaintyFile);
+  
+  JffCorrector *ppUncertaintyManager = new JffCorrector();
+  ppUncertaintyManager->ReadSystematicFile(ppUncertainryFile);
   
   const int nCentralityBins = dataManager->GetNCentralityBins();
   const int nTrackPtBins = dataManager->GetNTrackPtBins();
   const int nAsymmetryBins = dataManager->GetNAsymmetryBins();
-  double centralityBinBorders[] = {0,10,30,50,100};  // Bin borders for centrality
+  double centralityBinBorders[] = {0,10,30,50,90};  // Bin borders for centrality
   double trackPtBinBorders[] = {0.7,1,2,3,4,8,12,300};  // Bin borders for track pT
   double xjBinBorders[] = {0,0.6,0.8,1}; // Bin borders for xj
   
   // Define arrays for the jet shapes
-  TH1D *dataShape[2][nCentralityBins][nTrackPtBins];
-  TH1D *uncertaintyShape[2][JffCorrector::knUncertaintySources][nCentralityBins][nTrackPtBins];
+  TH1D *dataShape[2][nCentralityBins+1][nTrackPtBins+1];
+  TH1D *uncertaintyShape[2][JffCorrector::knUncertaintySources][nCentralityBins+1][nTrackPtBins+1];
   
   // Define arrays for yield
-  double dataYield[2][nCentralityBins][nTrackPtBins];
-  double uncertaintyYield[2][JffCorrector::knUncertaintySources][nCentralityBins][nTrackPtBins];
+  double dataYield[2][nCentralityBins+1][nTrackPtBins+1];
+  double uncertaintyYield[2][JffCorrector::knUncertaintySources][nCentralityBins+1][nTrackPtBins+1];
   
   
-  // Read the histograms from spillover file
-  for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
-    for(int iTrackPt = 0; iTrackPt < nTrackPtBins; iTrackPt++){
-      dataShape[0][iCentrality][iTrackPt] = dataManager->GetHistogramJetShape(DijetHistogramManager::kJetShape, DijetHistogramManager::kPtWeightedTrackLeadingJet, nAsymmetryBins, iCentrality, iTrackPt);
-      dataYield[0][iCentrality][iTrackPt] = dataShape[0][iCentrality][iTrackPt]->Integral(1, dataShape[0][iCentrality][iTrackPt]->FindBin(0.99), "width");
-      dataShape[1][iCentrality][iTrackPt] = dataManager->GetHistogramJetShape(DijetHistogramManager::kJetShape, DijetHistogramManager::kPtWeightedTrackSubleadingJet, nAsymmetryBins, iCentrality, iTrackPt);
-      dataYield[1][iCentrality][iTrackPt] = dataShape[1][iCentrality][iTrackPt]->Integral(1, dataShape[1][iCentrality][iTrackPt]->FindBin(0.99), "width");
-      for(int iUncertainty = 0; iUncertainty < JffCorrector::knUncertaintySources; iUncertainty++){
-        uncertaintyShape[0][iUncertainty][iCentrality][iTrackPt] = uncertaintyManager->GetJetShapeSystematicUncertainty(DijetHistogramManager::kPtWeightedTrackLeadingJet, iCentrality, iTrackPt, nAsymmetryBins, iUncertainty);
-        uncertaintyYield[0][iUncertainty][iCentrality][iTrackPt] = uncertaintyShape[0][iUncertainty][iCentrality][iTrackPt]->Integral(1, uncertaintyShape[0][iUncertainty][iCentrality][iTrackPt]->FindBin(0.99), "width");
-        uncertaintyShape[1][iUncertainty][iCentrality][iTrackPt] = uncertaintyManager->GetJetShapeSystematicUncertainty(DijetHistogramManager::kPtWeightedTrackSubleadingJet, iCentrality, iTrackPt, nAsymmetryBins, iUncertainty);
-        uncertaintyYield[1][iUncertainty][iCentrality][iTrackPt] = uncertaintyShape[1][iUncertainty][iCentrality][iTrackPt]->Integral(1, uncertaintyShape[1][iUncertainty][iCentrality][iTrackPt]->FindBin(0.99), "width");
+  // Read PbPb and pp histograms from files
+  for(int iCentrality = 0; iCentrality <= nCentralityBins; iCentrality++){
+    for(int iTrackPt = 0; iTrackPt <= nTrackPtBins; iTrackPt++){
+      
+      // Different data reader for pp and PbPb files
+      if(iCentrality == nCentralityBins){
+        dataShape[0][iCentrality][iTrackPt] = ppManager->GetHistogramJetShape(DijetHistogramManager::kJetShape, DijetHistogramManager::kPtWeightedTrackLeadingJet, nAsymmetryBins, 0, iTrackPt);
+        dataShape[1][iCentrality][iTrackPt] = ppManager->GetHistogramJetShape(DijetHistogramManager::kJetShape, DijetHistogramManager::kPtWeightedTrackSubleadingJet, nAsymmetryBins, 0, iTrackPt);
+      } else {
+        dataShape[0][iCentrality][iTrackPt] = dataManager->GetHistogramJetShape(DijetHistogramManager::kJetShape, DijetHistogramManager::kPtWeightedTrackLeadingJet, nAsymmetryBins, iCentrality, iTrackPt);
+        dataShape[1][iCentrality][iTrackPt] = dataManager->GetHistogramJetShape(DijetHistogramManager::kJetShape, DijetHistogramManager::kPtWeightedTrackSubleadingJet, nAsymmetryBins, iCentrality, iTrackPt);
       }
-    }
-  }
+      dataYield[0][iCentrality][iTrackPt] = dataShape[0][iCentrality][iTrackPt]->Integral(1, dataShape[0][iCentrality][iTrackPt]->FindBin(0.99), "width");
+      dataYield[1][iCentrality][iTrackPt] = dataShape[1][iCentrality][iTrackPt]->Integral(1, dataShape[1][iCentrality][iTrackPt]->FindBin(0.99), "width");
+      
+      for(int iUncertainty = 0; iUncertainty < JffCorrector::knUncertaintySources; iUncertainty++){
+        
+        // Different reader for pp and PbPb uncertainty files
+        if(iCentrality == nCentralityBins){
+          uncertaintyShape[0][iUncertainty][iCentrality][iTrackPt] = ppUncertaintyManager->GetJetShapeSystematicUncertainty(DijetHistogramManager::kPtWeightedTrackLeadingJet, 0, iTrackPt, nAsymmetryBins, iUncertainty);
+          uncertaintyShape[1][iUncertainty][iCentrality][iTrackPt] = ppUncertaintyManager->GetJetShapeSystematicUncertainty(DijetHistogramManager::kPtWeightedTrackSubleadingJet, 0, iTrackPt, nAsymmetryBins, iUncertainty);
+        } else {
+          uncertaintyShape[0][iUncertainty][iCentrality][iTrackPt] = uncertaintyManager->GetJetShapeSystematicUncertainty(DijetHistogramManager::kPtWeightedTrackLeadingJet, iCentrality, iTrackPt, nAsymmetryBins, iUncertainty);
+          uncertaintyShape[1][iUncertainty][iCentrality][iTrackPt] = uncertaintyManager->GetJetShapeSystematicUncertainty(DijetHistogramManager::kPtWeightedTrackSubleadingJet, iCentrality, iTrackPt, nAsymmetryBins, iUncertainty);
+        }
+        
+        uncertaintyYield[0][iUncertainty][iCentrality][iTrackPt] = uncertaintyShape[0][iUncertainty][iCentrality][iTrackPt]->Integral(1, uncertaintyShape[0][iUncertainty][iCentrality][iTrackPt]->FindBin(0.99), "width");
+        uncertaintyYield[1][iUncertainty][iCentrality][iTrackPt] = uncertaintyShape[1][iUncertainty][iCentrality][iTrackPt]->Integral(1, uncertaintyShape[1][iUncertainty][iCentrality][iTrackPt]->FindBin(0.99), "width");
+      } // Uncertainty type loop
+    } // Track pT loop
+  } // Centrality loop
   
   // Print a slide with uncertainties for each source and each centrality
   char namer[100];
-  for(int iTrackPt = 0; iTrackPt < nTrackPtBins; iTrackPt++){
-    
-    sprintf(namer,"\\frametitle{Subleading jet shape error, $%.1f < p_{\\mathrm{T}} < %.1f$}", trackPtBinBorders[iTrackPt], trackPtBinBorders[iTrackPt+1]);
-    
-    cout << endl;
-    cout << "\\begin{frame}" << endl;
-    cout << namer << endl;
-    cout << "\\begin{center}" << endl;
-    cout << "  \\begin{tabular}{ccccc}" << endl;
-    cout << "    \\toprule" << endl;
-    cout << "    Source & C: 0-10 & C: 10-30 & C: 30-50 & C: 50-100 \\\\" << endl;
-    cout << "    \\midrule" << endl;
-    
-    // Set the correct precision for printing floating point numbers
-    cout << fixed << setprecision(3);
-    
-    // Print one line for each track pT bin
-    for(int iUncertainty = 0; iUncertainty < JffCorrector::knUncertaintySources; iUncertainty++){
-      cout << uncertaintyManager->GetUncertaintyName(iUncertainty);
-      for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
-        cout << " & $" << uncertaintyYield[1][iUncertainty][iCentrality][iTrackPt] / dataYield[1][iCentrality][iTrackPt] << "$";
+  const char* jetType[2] = {"Leading","Subleading"};
+  for(int iJetType = 0; iJetType < 2; iJetType++){
+    for(int iTrackPt = 0; iTrackPt <= nTrackPtBins; iTrackPt++){
+      
+      if(iTrackPt == nTrackPtBins){
+        sprintf(namer,"\\frametitle{%s jet shape error, $p_{\\mathrm{T}}$} integrated}",jetType[iJetType]);
+      } else {
+        sprintf(namer,"\\frametitle{%s jet shape error, $%.1f < p_{\\mathrm{T}} < %.1f$}",jetType[iJetType], trackPtBinBorders[iTrackPt], trackPtBinBorders[iTrackPt+1]);
       }
-      cout << " \\\\" << endl;
+      
+      cout << endl;
+      cout << "\\begin{frame}" << endl;
+      cout << namer << endl;
+      cout << "\\begin{center}" << endl;
+      cout << "  \\begin{tabular}{cccccc}" << endl;
+      cout << "    \\toprule" << endl;
+      cout << "    Source & C: 0-10 & C: 10-30 & C: 30-50 & C: 50-90 & pp \\\\" << endl;
+      cout << "    \\midrule" << endl;
+      
+      // Set the correct precision for printing floating point numbers
+      cout << fixed << setprecision(3);
+      
+      // Print one line for each track pT bin
+      for(int iUncertainty = 0; iUncertainty < JffCorrector::knUncertaintySources; iUncertainty++){
+        cout << uncertaintyManager->GetUncertaintyName(iUncertainty);
+        for(int iCentrality = 0; iCentrality <= nCentralityBins; iCentrality++){
+          cout << " & $" << uncertaintyYield[iJetType][iUncertainty][iCentrality][iTrackPt] / dataYield[iJetType][iCentrality][iTrackPt] << "$";
+        }
+        cout << " \\\\" << endl;
+      }
+      
+      cout << "    \\bottomrule" << endl;
+      cout << "  \\end{tabular}" << endl;
+      cout << "\\end{center}" << endl;
+      cout << "\\end{frame}" << endl;
+      cout << endl;
+      
     }
-    
-    cout << "    \\bottomrule" << endl;
-    cout << "  \\end{tabular}" << endl;
-    cout << "\\end{center}" << endl;
-    cout << "\\end{frame}" << endl;
-    cout << endl;
-    
   }
 }
 
