546ca10f82c82f79511ea6e1343684e11e4c642f
Jussi Viinikainen <jviinika@uic.edu>
2019-10-07 16:36:41 -0500

    Added scaling of distributions to this macro

--- plotting/produceTrackingDeltaRCorrection.C
+++ plotting/produceTrackingDeltaRCorrection.C
@@ -11,14 +11,16 @@ void produceTrackingDeltaRCorrection(){
   // ==================================================================
   // ========================= Configuration ==========================
   // ==================================================================
+    
+  TString genRecoFileName = "data/ppMC2017_GenReco_Pythia8_pfJets_wtaAxis_noUncorr_20EventsMixed_JECv4_processed_2019-09-28.root";  // File from which the GenReco histograms are read for the correction
+  // data/PbPbMC_GenReco_akFlowPuCs4PFJet_xjBins_allHistograms_improvisedMixing_wtaAxis_finalTrack_noCorrections_processed_2019-09-28.root
+  // data/ppMC2017_GenReco_Pythia8_pfJets_wtaAxis_allHistograms_improvisedMixing_processed_2019-09-26.root
   
-  bool yieldQA = false;     // Print out relative yields between uncorrected data and spillover distribution
+  TString gengenFileName = "data/ppMC2017_GenGen_Pythia8_pfJets_wtaAxis_noUncorr_20EventsMixed_JECv4_processed_2019-09-28.root"; // File from which the GenGen histograms are read for the correction
+  // data/PbPbMC_GenGen_akFlowPuCs4PFJet_noUncorr_improvisedMixing_xjBins_wtaAxis_JECv6_processed_2019-09-24.root
+  // data/ppMC2017_GenGen_Pythia8_pfJets_wtaAxis_allHistograms_improvisedMixing_processed_2019-09-26.root
   
-  TString genRecoFileName = "data/PbPbMC_GenReco_akFlowPuCs4PFJet_noUncorr_improvisedMixing_wtaAxis_adhocScaling_noCorrections_JECv6_processed_2019-09-24.root";  // File from which the GenReco histograms are read for the correction
-  
-  TString gengenFileName = "data/PbPbMC_GenGen_akFlowPuCs4PFJet_noUncorr_improvisedMixing_wtaAxis_noCorrections_JECv6_processed_2019-09-24.root"; // File from which the GenGen histograms are read for the correction
-  
-  TString outputFileName = "data/trackDeltaRCorrection_PbPbMC_noUncorr_improvisedMixing_2019-09-24.root"; // File name for the output file
+  TString outputFileName = "testTrackingPpAgain.root"; // File name for the output file
   
   bool regularJetTrack = true;       // Produce the correction for reguler jet-track correlations
   bool uncorrectedJetTrack = false;  // Produce the correction for uncorrected jet-track correlations
@@ -52,15 +54,26 @@ void produceTrackingDeltaRCorrection(){
   }
   
   // Find the correct number of centrality and track pT bins
-  const int nCentralityBins = histograms[0]->GetNCentralityBins();
+  int nCentralityBins = histograms[0]->GetNCentralityBins();
   const int nTrackPtBins = histograms[0]->GetNTrackPtBins();
   const int nAsymmetryBins = histograms[0]->GetNAsymmetryBins();
+  
+  if(histograms[0]->GetSystem().Contains("pp",TString::kIgnoreCase)) nCentralityBins = 1;
   
   // Initialize correction histograms and helper histograms
   TH2D *genRecoDeltaEtaDeltaPhi[DijetHistogramManager::knJetTrackCorrelations][nAsymmetryBins+1][nCentralityBins][nTrackPtBins];
   TH2D *genGenDeltaEtaDeltaPhi[DijetHistogramManager::knJetTrackCorrelations][nAsymmetryBins+1][nCentralityBins][nTrackPtBins];
   TH2D *symmetrizedCorrectionDeltaEtaDeltaPhi[DijetHistogramManager::knJetTrackCorrelations][nAsymmetryBins+1][nCentralityBins][nTrackPtBins];
+  TH2D *symmetrizedHelperDeltaEtaDeltaPhi[DijetHistogramManager::knJetTrackCorrelations][nAsymmetryBins+1][nCentralityBins][nTrackPtBins];
+  TH1D *genRecoDeltaEtaProjection[DijetHistogramManager::knJetTrackCorrelations][nAsymmetryBins+1][nCentralityBins][nTrackPtBins];
+  TH1D *genGenDeltaEtaProjection[DijetHistogramManager::knJetTrackCorrelations][nAsymmetryBins+1][nCentralityBins][nTrackPtBins];
 
+  // Functions to fit deltaEta projections needed to obtain scaling factors to match genReco and genGen at large angles
+  TF1 *constantMinus = new TF1("coMi","pol0",-2.5,-0.5);
+  TF1 *constantPlus = new TF1("coPl","pol0",0.5,2.5);
+  double scalingFactor = 1;
+  double maxDeltaR = 0.4;  // Maximum radius to which the correction is applied
+  
   // Initialize the arrays to NULL
   for(int iJetTrack = 0; iJetTrack < DijetHistogramManager::knJetTrackCorrelations; iJetTrack++){
     
@@ -72,6 +85,9 @@ void produceTrackingDeltaRCorrection(){
           genRecoDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = NULL;
           genGenDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = NULL;
           symmetrizedCorrectionDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = NULL;
+          symmetrizedHelperDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = NULL;
+          genRecoDeltaEtaProjection[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = NULL;
+          genGenDeltaEtaProjection[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = NULL;
         } // Asymmetry loop
       } // Track pT loop
     } // Centrality loop
@@ -94,17 +110,40 @@ void produceTrackingDeltaRCorrection(){
           // No asymmetry binning for inclusive jet-track correlations
           if(iJetTrack >= DijetHistogramManager::kTrackInclusiveJet && iAsymmetry != nAsymmetryBins) continue;
           
+          // Read the near side deltaEta projections to do scaling
+          genRecoDeltaEtaProjection[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = histograms[0]->GetHistogramJetTrackDeltaEta(iJetTrack, DijetHistogramManager::kCorrected, iAsymmetry, iCentrality, iTrackPt, DijetHistogramManager::kNearSide);
+          
+          genGenDeltaEtaProjection[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = histograms[1]->GetHistogramJetTrackDeltaEta(iJetTrack, DijetHistogramManager::kCorrected, iAsymmetry, iCentrality, iTrackPt, DijetHistogramManager::kNearSide);
+          
+          // Take a ratio of deltaEta histograms and fit the tails to get a scaling factor for the correction
+          genRecoDeltaEtaProjection[iJetTrack][iAsymmetry][iCentrality][iTrackPt]->Divide(genGenDeltaEtaProjection[iJetTrack][iAsymmetry][iCentrality][iTrackPt]);
+          genRecoDeltaEtaProjection[iJetTrack][iAsymmetry][iCentrality][iTrackPt]->Fit(constantMinus,"","",-2.5,-0.5);
+          genRecoDeltaEtaProjection[iJetTrack][iAsymmetry][iCentrality][iTrackPt]->Fit(constantPlus,"","",0.5,2.5);
+          scalingFactor = 2.0/(constantMinus->GetParameter(0)+constantPlus->GetParameter(0));
+          
+          // Since for large pT bins the GenReco and GenGen agree within uncertainties, do not scale them to match.
+          // Also for large pT use smaller area around the jet peak for the correction
+          if(iTrackPt > 2) {
+            scalingFactor = 1;
+            maxDeltaR = 0.4;
+          } else {
+            maxDeltaR = 0.4;
+          }
+          
           // Read the GenReco histogram
           genRecoDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = histograms[0]->GetHistogramJetTrackDeltaEtaDeltaPhi(iJetTrack, DijetHistogramManager::kCorrected, iAsymmetry, iCentrality, iTrackPt);
+          genRecoDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt]->Scale(scalingFactor);
           
           // Read the GenGen histogram
           genGenDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = histograms[1]->GetHistogramJetTrackDeltaEtaDeltaPhi(iJetTrack, DijetHistogramManager::kCorrected, iAsymmetry, iCentrality, iTrackPt);
           
-          // Divide the GenReco histogram with the GenGen histogram to get the correction
-          genRecoDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt]->Divide(genGenDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt]);
+          // Symmetrize the distributions before taking the ratio!!!!
+          symmetrizedCorrectionDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = corrector->SymmetrizeHistogram(genRecoDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt], maxDeltaR, 1);
           
-          // Symmetrize the ratio histogram to get the correction. Only apply the correction close to the jet axis (R < 0.4)
-          symmetrizedCorrectionDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = corrector->SymmetrizeHistogram(genRecoDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt], 0.4, 1);
+          symmetrizedHelperDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt] = corrector->SymmetrizeHistogram(genGenDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt], maxDeltaR, 1);
+          
+          // Divide the GenReco histogram with the GenGen histogram to get the correction
+          symmetrizedCorrectionDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt]->Divide(symmetrizedHelperDeltaEtaDeltaPhi[iJetTrack][iAsymmetry][iCentrality][iTrackPt]);
           
         } // Asymmetry loop
       } // Track pT loop
