e97a704e2ea88f6ef9036e551209d6af0e3d65a4
Jussi Viinikainen <jviinika@uic.edu>
2021-05-05 11:12:32 -0500

    Configuration updates

--- preRunnerInSteps.sh
+++ preRunnerInSteps.sh
@@ -6,7 +6,7 @@
 #./processHistogramsInStepsAsymmetryPp.sh "data/" "data/" false false -1
 #./processHistogramsInStepsManual.sh "data/" "data/" false false -1 "data/"
 
-./processHistogramsInSteps.sh "data/PbPbMC2018_GenGen_akCaloJet_dihadron_noCentShift_improvisedMixing_subeNon0_noXj_preprocessed_2021-04-27.root" "data/PbPbMC2018_GenGen_akCaloJet_dihadron_noCentShift_improvisedMixing_subeNon0_noXj_noCorrections_processed_2021-04-27.root" false false -1 "data/"
+./processHistogramsInSteps.sh "data/PbPbMC2018_GenGen_akCaloJet_dihadron_noCentShift_improvisedMixing_subeNon0_noXj_preprocessed_2021-04-27.root" "data/PbPbMC2018_GenGen_akCaloJet_dihadron_noCentShift_improvisedMixing_subeNon0_noXj_wideDeltaEta_noCorrections_processed_2021-04-27.root" false false -1 "data/"
 
 #./processHistogramsInSteps.sh "data/PbPbMC2018_RecoGen_akFlowJet_dihadron_4pCentShift_subeNon0_improvisedMixing_noXj_noQcut_preprocessed_2021-04-08.root" "data/PbPbMC2018_RecoGen_akFlowJet_dihadron_4pCentShift_subeNon0_improvisedMixing_noXj_noQcut_noCorrections_processed_2021-04-08.root" false false -1 "data/"
 #./processHistogramsInSteps.sh "data/PbPbMC2018_RecoGen_akFlowJet_dihadron_4pCentShift_subeNon0_improvisedMixing_noXj_qVectorBelow1p5_preprocessed_2021-04-08.root" "data/PbPbMC2018_RecoGen_akFlowJet_dihadron_4pCentShift_subeNon0_improvisedMixing_noXj_qVectorBelow1p5_noCorrections_processed_2021-04-08.root" false false -1 "data/"
