db89090d5c8a5b4a25ad70a7cc45ed80c7da1854
Jussi Viinikainen <jviinika@uic.edu>
2021-09-14 17:52:00 -0500

    Added examples of dihadron configuration

---
+++ crab/crabDijetPbPbDihadron.py
@@ -0,0 +1,42 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+card='cardDijetPbPbDihadron.input'
+jobTag='dihadronPbPb2018_sameTriggerAssoc_caloDijet_smearedJER_5eventMixed_2021-08-11_part0'
+#inputList='PbPbData2018_MinBiasFiles.txt'
+inputList='PbPbData2018_flowJetSkims_80and100triggers_part0.txt'
+outputFile=jobTag+'.root'
+fileLocation='1'  # Locations: 0 = Purdue, 1 = CERN, 2 = Search with xrootd
+
+config.section_("General")
+config.General.requestName = jobTag
+config.General.workArea = config.General.requestName 
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PSet.py'
+config.JobType.scriptExe = 'compileAndRun.sh'
+config.JobType.scriptArgs = ['card='+card,'output='+outputFile,'location='+fileLocation]
+config.JobType.inputFiles = ['FrameworkJobReport.xml','dijet5TeV.tar.gz',card]
+config.JobType.outputFiles = [outputFile]
+config.JobType.maxJobRuntimeMin = 1440
+config.JobType.maxMemoryMB = 2000
+
+config.section_("Data")
+config.Data.userInputFiles = open(inputList).readlines() 
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = len(config.Data.userInputFiles)
+config.Data.outputPrimaryDataset = 'diJetPbPbHistograms'
+config.Data.outLFNDirBase = '/store/user/jviinika/'+config.General.requestName
+config.Data.publication = False
+
+config.section_("Site")
+config.Site.whitelist = ['T2_CH_CERN']
+#config.Site.whitelist = ['T2_US_Purdue','T2_US_Wisconsin','T2_US_Nebraska']
+config.Site.storageSite = 'T3_US_FNALLPC'
+
+#"really" force crab to only run at whitelisted sites
+config.section_("Debug")
+config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
+
