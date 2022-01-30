5b9e813de76d2c897c05f9993b0c5906acec2d39
Jussi Viinikainen <jussi_viinikainen@hotmail.com>
2022-01-30 14:34:10 -0600

    Added option to add several histograms in same plot

--- plotting/longRangeSystematicExplorer.C
+++ plotting/longRangeSystematicExplorer.C
@@ -12,7 +12,12 @@ void longRangeSystematicExplorer(){
   
   // Input file name for uncertainties
   TString directoryName = "flowGraphs/";
-  TString uncertaintyFileName = "systematicUncertainties_allSources_finalCorrection_2021-08-10.root";
+  const int nFiles = 2;
+  TString uncertaintyFileName[] = {"systematicUncertainties_allSources_finalCorrection_2021-08-10.root", "systematicUncertainties_addMultiplicityMatch_noCentralityShift_finalCorrection_2022-28-01.root"};
+  // systematicUncertainties_addMultiplicityMatch_noCentralityShift_finalCorrection_2022-28-01.root
+  // systematicUncertainties_allSources_finalCorrection_2021-08-10.root
+  
+  TString legendComment[] = {"Centrality shift", "Multiplicity match", "Dummy", "Dummy"};
   
   // Define the bins that are drawn
   const int nCentralityBins = 3;  // Number of drawn centrality bins
@@ -27,88 +32,99 @@ void longRangeSystematicExplorer(){
   const bool drawAbsoluteUncertainties = true;
   
   // Save the final plots
-  const bool saveFigures = false;
-  TString saveComment = "_newSources";
+  const bool saveFigures = true;
+  TString saveComment = "_uncertaintyComparison";
+  TString fileFormat = "pdf";
   
   // =========== //
   // Read graphs //
   // =========== //
   
-  TGraphErrors *jetVnUncertainty[LongRangeSystematicOrganizer::knUncertaintySources][maxVn];
+  TGraphErrors *jetVnUncertainty[nFiles][LongRangeSystematicOrganizer::knUncertaintySources][maxVn];
   
   // Initialize the graphs to NULL
-  for(int iUncertainty = 0; iUncertainty < LongRangeSystematicOrganizer::knUncertaintySources; iUncertainty++){
-    for(int iFlow = 0; iFlow < maxVn; iFlow++){
-      jetVnUncertainty[iUncertainty][iFlow] = NULL;
-    } // Flow component loop
-  } // Uncertainty loop
+  for(int iFile = 0; iFile < nFiles; iFile++){
+    for(int iUncertainty = 0; iUncertainty < LongRangeSystematicOrganizer::knUncertaintySources; iUncertainty++){
+      for(int iFlow = 0; iFlow < maxVn; iFlow++){
+        jetVnUncertainty[iFile][iUncertainty][iFlow] = NULL;
+      } // Flow component loop
+    } // Uncertainty loop
+  } // File loop
   
   // Read the systematic uncertainty graphs
-  TFile *uncertaintyFile = TFile::Open(directoryName+uncertaintyFileName);
-  LongRangeSystematicOrganizer *uncertaintyOrganizer = new LongRangeSystematicOrganizer(uncertaintyFile);
-  
-  for(int iUncertainty = 0; iUncertainty < LongRangeSystematicOrganizer::knUncertaintySources; iUncertainty++){
-    for(int iFlow = firstDrawnVn-1; iFlow <= lastDrawnVn-1; iFlow++){
-      jetVnUncertainty[iUncertainty][iFlow] = uncertaintyOrganizer->GetLongRangeSystematicUncertainty(iFlow, iUncertainty);
-      
-      // TODO: Maybe some error handling if some uncertainties are not included in the file
-      if(jetVnUncertainty[iUncertainty][iFlow] == NULL){
-        cout << "So, this is awkward..." << endl;
-        cout << "It seems that I could not find the graph for v" << iFlow+1 << " " << uncertaintyOrganizer->GetLongRangeUncertaintyName(iUncertainty) << endl;
-      }
-      
-    } // Flow component loop
-  } // Uncertainty loop
+  TFile *uncertaintyFile[nFiles];
+  LongRangeSystematicOrganizer *uncertaintyOrganizer[nFiles];
+  for(int iFile = 0; iFile < nFiles; iFile++){
+    uncertaintyFile[iFile] = TFile::Open(directoryName+uncertaintyFileName[iFile]);
+    uncertaintyOrganizer[iFile] = new LongRangeSystematicOrganizer(uncertaintyFile[iFile]);
+    
+    for(int iUncertainty = 0; iUncertainty < LongRangeSystematicOrganizer::knUncertaintySources; iUncertainty++){
+      for(int iFlow = firstDrawnVn-1; iFlow <= lastDrawnVn-1; iFlow++){
+        jetVnUncertainty[iFile][iUncertainty][iFlow] = uncertaintyOrganizer[iFile]->GetLongRangeSystematicUncertainty(iFlow, iUncertainty);
+        
+        // TODO: Maybe some error handling if some uncertainties are not included in the file
+        if(jetVnUncertainty[iFile][iUncertainty][iFlow] == NULL){
+          cout << "So, this is awkward..." << endl;
+          cout << "It seems that I could not find the graph for v" << iFlow+1 << " " << uncertaintyOrganizer[iFile]->GetLongRangeUncertaintyName(iUncertainty) << endl;
+        }
+        
+      } // Flow component loop
+    } // Uncertainty loop
+  } // File loop
   
   // ============================================================================================== //
   // Transform the uncertainty graphs into histograms illustrating different sources of uncertainty //
   // ============================================================================================== //
   
-  TH1D* systematicIllustrationAbsolute[maxVn][nCentralityBins];
-  TH1D* systematicIllustrationRelative[maxVn][nCentralityBins];
+  TH1D* systematicIllustrationAbsolute[nFiles][maxVn][nCentralityBins];
+  TH1D* systematicIllustrationRelative[nFiles][maxVn][nCentralityBins];
   TString histogramName;
   
   // Initilize the histograms
-  for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
-    for(int iFlow = 0; iFlow < maxVn; iFlow++){
-      
-      histogramName = Form("systematicIllustrationAbsolute_v%d_C%d", iFlow+1, iCentrality);
-      systematicIllustrationAbsolute[iFlow][iCentrality] = new TH1D(histogramName, histogramName, LongRangeSystematicOrganizer::knUncertaintySources, -0.5, LongRangeSystematicOrganizer::knUncertaintySources-0.5);
-      systematicIllustrationAbsolute[iFlow][iCentrality]->Sumw2();
-      
-      histogramName = Form("systematicIllustrationRelative_v%d_C%d", iFlow+1, iCentrality);
-      systematicIllustrationRelative[iFlow][iCentrality] = new TH1D(histogramName, histogramName, LongRangeSystematicOrganizer::knUncertaintySources, -0.5, LongRangeSystematicOrganizer::knUncertaintySources-0.5);
-      systematicIllustrationRelative[iFlow][iCentrality]->Sumw2();
-      
-      for(int iUncertainty = 0; iUncertainty < LongRangeSystematicOrganizer::knUncertaintySources; iUncertainty++){
+  for(int iFile = 0; iFile < nFiles; iFile++){
+    for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
+      for(int iFlow = 0; iFlow < maxVn; iFlow++){
         
-        // Change the bin labels to refer to different sources of uncertainty
-        systematicIllustrationAbsolute[iFlow][iCentrality]->GetXaxis()->ChangeLabel(iUncertainty+1, 315, -1, -1, -1, -1, uncertaintyOrganizer->GetUncertaintyAxisName(iUncertainty));
-        systematicIllustrationRelative[iFlow][iCentrality]->GetXaxis()->ChangeLabel(iUncertainty+1, 315, -1, -1, -1, -1, uncertaintyOrganizer->GetUncertaintyAxisName(iUncertainty));
+        histogramName = Form("systematicIllustrationAbsolute%d_v%d_C%d", iFile, iFlow+1, iCentrality);
+        systematicIllustrationAbsolute[iFile][iFlow][iCentrality] = new TH1D(histogramName, histogramName, LongRangeSystematicOrganizer::knUncertaintySources, -0.5, LongRangeSystematicOrganizer::knUncertaintySources-0.5);
+        systematicIllustrationAbsolute[iFile][iFlow][iCentrality]->Sumw2();
         
-      } // Uncertainty loop
-    } // Flow component loop
-  } // Centrality loop
+        histogramName = Form("systematicIllustrationRelative%d_v%d_C%d", iFile, iFlow+1, iCentrality);
+        systematicIllustrationRelative[iFile][iFlow][iCentrality] = new TH1D(histogramName, histogramName, LongRangeSystematicOrganizer::knUncertaintySources, -0.5, LongRangeSystematicOrganizer::knUncertaintySources-0.5);
+        systematicIllustrationRelative[iFile][iFlow][iCentrality]->Sumw2();
+        
+        for(int iUncertainty = 0; iUncertainty < LongRangeSystematicOrganizer::knUncertaintySources; iUncertainty++){
+          
+          // Change the bin labels to refer to different sources of uncertainty
+          systematicIllustrationAbsolute[iFile][iFlow][iCentrality]->GetXaxis()->ChangeLabel(iUncertainty+1, 315, -1, -1, -1, -1, uncertaintyOrganizer[iFile]->GetUncertaintyAxisName(iUncertainty));
+          systematicIllustrationRelative[iFile][iFlow][iCentrality]->GetXaxis()->ChangeLabel(iUncertainty+1, 315, -1, -1, -1, -1, uncertaintyOrganizer[iFile]->GetUncertaintyAxisName(iUncertainty));
+          
+        } // Uncertainty loop
+      } // Flow component loop
+    } // Centrality loop
+  } // File loop
   
   double valueY, errorY, ratioY;
   
   // Fill the histograms
-  for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
-    for(int iFlow = firstDrawnVn-1; iFlow <= lastDrawnVn-1; iFlow++){
-      for(int iUncertainty = 0; iUncertainty < LongRangeSystematicOrganizer::knUncertaintySources; iUncertainty++){
-        
-        // For the absolute uncertainty, read the number directly from the uncertainty histogram
-        errorY = jetVnUncertainty[iUncertainty][iFlow]->GetErrorY(iCentrality);
-        systematicIllustrationAbsolute[iFlow][iCentrality]->SetBinContent(iUncertainty+1,errorY);
-        
-        // For the relative uncertainty, calculate the ratio between uncertainty and central value
-        jetVnUncertainty[iUncertainty][iFlow]->GetPoint(iCentrality, ratioY, valueY);
-        ratioY = errorY / TMath::Abs(valueY);
-        systematicIllustrationRelative[iFlow][iCentrality]->SetBinContent(iUncertainty+1,ratioY);
-        
-      } // Uncertainty loop
-    } // Flow component loop
-  } // Centrality loop
+  for(int iFile = 0; iFile < nFiles; iFile++){
+    for(int iCentrality = 0; iCentrality < nCentralityBins; iCentrality++){
+      for(int iFlow = firstDrawnVn-1; iFlow <= lastDrawnVn-1; iFlow++){
+        for(int iUncertainty = 0; iUncertainty < LongRangeSystematicOrganizer::knUncertaintySources; iUncertainty++){
+          
+          // For the absolute uncertainty, read the number directly from the uncertainty histogram
+          errorY = jetVnUncertainty[iFile][iUncertainty][iFlow]->GetErrorY(iCentrality);
+          systematicIllustrationAbsolute[iFile][iFlow][iCentrality]->SetBinContent(iUncertainty+1,errorY);
+          
+          // For the relative uncertainty, calculate the ratio between uncertainty and central value
+          jetVnUncertainty[iFile][iUncertainty][iFlow]->GetPoint(iCentrality, ratioY, valueY);
+          ratioY = errorY / TMath::Abs(valueY);
+          systematicIllustrationRelative[iFile][iFlow][iCentrality]->SetBinContent(iUncertainty+1,ratioY);
+          
+        } // Uncertainty loop
+      } // Flow component loop
+    } // Centrality loop
+  } // File loop
   
   // ============== //
   // Draw the plots //
@@ -127,6 +143,8 @@ void longRangeSystematicExplorer(){
   
   TLegend *legend;
   double maxValue;
+  double highValue;
+  int lineColor[] = {kBlue, kRed, kCyan, kMagenta, kGreen+3};
   
   // Draw the relative uncertainties
   if(drawRelativeUncertainties){
@@ -136,18 +154,32 @@ void longRangeSystematicExplorer(){
         legend = new TLegend(0.11,0.7,0.41,0.9);
         legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
         
-        maxValue = systematicIllustrationRelative[iFlow][iCentrality]->GetBinContent(LongRangeSystematicOrganizer::kAll+1);
-        maxValue = maxValue + 0.1 * maxValue;
-        systematicIllustrationRelative[iFlow][iCentrality]->GetYaxis()->SetRangeUser(0,maxValue);
+        // Find the maximum values over all drawn histograms
+        maxValue = 0;
+        for(int iFile = 0; iFile < nFiles; iFile++){
+          systematicIllustrationRelative[iFile][iFlow][iCentrality]->SetLineColor(lineColor[iFile]);
+          highValue = systematicIllustrationRelative[iFile][iFlow][iCentrality]->GetBinContent(LongRangeSystematicOrganizer::kAll+1);
+          highValue = highValue + 0.1 * highValue;
+          if(highValue > maxValue) maxValue = highValue;
+        }
+        systematicIllustrationRelative[0][iFlow][iCentrality]->GetYaxis()->SetRangeUser(0,maxValue);
         
-        drawer->DrawHistogram(systematicIllustrationRelative[iFlow][iCentrality], "Source", "Relative uncertainty", " ");
+        drawer->DrawHistogram(systematicIllustrationRelative[0][iFlow][iCentrality], "Source", "Relative uncertainty", " ");
+        for(int iFile = 1; iFile < nFiles; iFile++){
+          systematicIllustrationRelative[iFile][iFlow][iCentrality]->Draw("same");
+        }
         
         legend->AddEntry((TObject*) 0, Form("Cent: %.0f-%.0f%%", centralityBinBorders[iCentrality], centralityBinBorders[iCentrality+1]), "");
+        if(nFiles > 1){
+          for(int iFile = 0; iFile < nFiles; iFile++){
+            legend->AddEntry(systematicIllustrationRelative[iFile][iFlow][iCentrality], legendComment[iFile], "l");
+          }
+        }
         legend->Draw();
         
         // Save the figures to file
         if(saveFigures){
-          gPad->GetCanvas()->SaveAs(Form("figures/relativeUncertainties%s_jetV%d_C=%.0f-%.0f.pdf", saveComment.Data(), iFlow+1, centralityBinBorders[iCentrality], centralityBinBorders[iCentrality+1]));
+          gPad->GetCanvas()->SaveAs(Form("figures/relativeUncertainties%s_jetV%d_C=%.0f-%.0f.%s", saveComment.Data(), iFlow+1, centralityBinBorders[iCentrality], centralityBinBorders[iCentrality+1], fileFormat.Data()));
         }
       } // Centrality loop
     } // Flow component loop
@@ -161,18 +193,32 @@ void longRangeSystematicExplorer(){
         legend = new TLegend(0.11,0.7,0.41,0.9);
         legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
         
-        maxValue = systematicIllustrationAbsolute[iFlow][iCentrality]->GetBinContent(LongRangeSystematicOrganizer::kAll+1);
-        maxValue = maxValue + 0.1 * maxValue;
-        systematicIllustrationAbsolute[iFlow][iCentrality]->GetYaxis()->SetRangeUser(0,maxValue);
+        // Find the maximum values over all drawn histograms
+        maxValue = 0;
+        for(int iFile = 0; iFile < nFiles; iFile++){
+          systematicIllustrationAbsolute[iFile][iFlow][iCentrality]->SetLineColor(lineColor[iFile]);
+          highValue = systematicIllustrationAbsolute[iFile][iFlow][iCentrality]->GetBinContent(LongRangeSystematicOrganizer::kAll+1);
+          highValue = highValue + 0.1 * highValue;
+          if(highValue > maxValue) maxValue = highValue;
+        }
+        systematicIllustrationAbsolute[0][iFlow][iCentrality]->GetYaxis()->SetRangeUser(0,maxValue);
         
-        drawer->DrawHistogram(systematicIllustrationAbsolute[iFlow][iCentrality], "Source", "Absolute uncertainty", " ");
+        drawer->DrawHistogram(systematicIllustrationAbsolute[0][iFlow][iCentrality], "Source", "Absolute uncertainty", " ");
+        for(int iFile = 1; iFile < nFiles; iFile++){
+          systematicIllustrationAbsolute[iFile][iFlow][iCentrality]->Draw("same");
+        }
         
         legend->AddEntry((TObject*) 0, Form("Cent: %.0f-%.0f%%", centralityBinBorders[iCentrality], centralityBinBorders[iCentrality+1]), "");
+        if(nFiles > 1){
+          for(int iFile = 0; iFile < nFiles; iFile++){
+            legend->AddEntry(systematicIllustrationAbsolute[iFile][iFlow][iCentrality], legendComment[iFile], "l");
+          }
+        }
         legend->Draw();
         
         // Save the figures to file
         if(saveFigures){
-          gPad->GetCanvas()->SaveAs(Form("figures/absoluteUncertainties%s_jetV%d_C=%.0f-%.0f.pdf", saveComment.Data(), iFlow+1, centralityBinBorders[iCentrality], centralityBinBorders[iCentrality+1]));
+          gPad->GetCanvas()->SaveAs(Form("figures/absoluteUncertainties%s_jetV%d_C=%.0f-%.0f.%s", saveComment.Data(), iFlow+1, centralityBinBorders[iCentrality], centralityBinBorders[iCentrality+1], fileFormat.Data()));
         }
       } // Centrality loop
     } // Flow component loop
