bf8cd91aa8639644f2e0c7532408da1ca99f9ee1
Jussi Viinikainen <jussi_viinikainen@hotmail.com>
2022-02-16 08:30:17 -0600

    Configuration updates

--- preRunnerInSteps.sh
+++ preRunnerInSteps.sh
@@ -6,7 +6,8 @@
 #./processHistogramsInStepsAsymmetryPp.sh "data/" "data/" false false -1
 #./processHistogramsInStepsManual.sh "data/" "data/" false false -1 "data/"
 
-./processHistogramsInSteps.sh "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_multWeight_subeNon0_qWeights_jetEta1v3_improvisedMixing_preprocessed_2022-02-09.root" "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_multWeight_subeNon0_qWeights_jetEta1v3_improvisedMixing_noCorrections_processed_2022-02-09.root" false false -1 "data/"
+./processHistogramsInSteps.sh "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_subeNon0_multWeight_jetEta1v3_onlyGluonJets_improvisedMixing_preprocessed_2022-01-24.root" "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_subeNon0_multWeight_jetEta1v3_onlyGluonJets_improvisedMixing_noCorrections_processed_2022-01-24.root" false false -1 "data/"
+./processHistogramsInSteps.sh "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_subeNon0_multWeight_jetEta1v3_onlyQuarkJets_improvisedMixing_preprocessed_2022-01-24.root" "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_subeNon0_multWeight_jetEta1v3_onlyQuarkJets_improvisedMixing_noCorrections_processed_2022-01-24.root" false false -1 "data/"
 
 #./processHistogramsInSteps.sh "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_4pCentShift_subeNon0_improvisedMixing_quarkJets_preprocessed_2021-03-02.root" "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_4pCentShift_subeNon0_improvisedMixing_quarkJets_noCorrections_processed_2021-03-02.root" false false -1 "data/"
 #./processHistogramsInSteps.sh "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_4pCentShift_subeNon0_improvisedMixing_gluonJets_preprocessed_2021-03-02.root" "data/PbPbMC2018_RecoGen_akCaloJet_onlyRegular_4pCentShift_subeNon0_improvisedMixing_gluonJets_noCorrections_processed_2021-03-02.root" false false -1 "data/"
