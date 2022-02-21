1836c7629fc17cfeeeb22d8b4a27af1b3371a122
Jussi Viinikainen <jussi_viinikainen@hotmail.com>
2022-02-21 16:41:03 -0600

    Improved drawing, added ratio

--- plotting/fitJetEventPlaneVnMultiplicity.C
+++ plotting/fitJetEventPlaneVnMultiplicity.C
@@ -4,14 +4,20 @@
 void fitJetEventPlaneVnMultiplicity(){
 
   // Open the data files
-  const int nFiles = 1;
+  const int nFiles = 2;
   TFile *inputFile[nFiles];
-  inputFile[0] = TFile::Open("eventPlaneCorrelation/jetEventPlaneDeltaPhi_PbPbMC2018_caloJets_subeNon0_multiplicityWeight_allEventPlanes_jetEta1v3_2022-01-30.root");
-  if(nFiles > 1) inputFile[1] = TFile::Open("eventPlaneCorrelation/jetEventPlaneDeltaPhi_PbPbMC2018_caloJets_multiplicityBins_manualEventPlane_jetEta1v3_2022-01-25.root");
-  if(nFiles > 2) inputFile[2] = TFile::Open("eventPlaneCorrelation/jetEventPlaneDeltaPhi_PbPbMC2018_caloJets_multiplicityBins_manualEventPlaneV3_jetEta1v3_2022-01-26.root");
+  inputFile[0] = TFile::Open("eventPlaneCorrelation/jetEventPlaneDeltaPhi_PbPbMC2018_caloJets_multWeight_allEventPlanes_jetEta1v3_2022-02-17.root");
+  if(nFiles > 1) inputFile[1] = TFile::Open("eventPlaneCorrelation/jetEventPlaneDeltaPhi_PbPbMC2018_pfCsJets_multWeight_allEventPlanes_jetEta1v6_2022-02-17.root");
+  if(nFiles > 2) inputFile[2] = TFile::Open("eventPlaneCorrelation/jetEventPlaneDeltaPhi_PbPbMC2018_caloJets_multWeight_forestEventPlanes_jetEta1v3_2022-02-18.root");
   if(nFiles > 3) inputFile[3] = TFile::Open("eventPlaneCorrelation/jetEventPlaneDeltaPhi_PbPbMC2018_caloJets_multiplicityBins_manualEventPlaneV4_jetEta1v3_2022-01-26.root");
+  // jetEventPlaneDeltaPhi_PbPb2018_caloJets_forestEventPlanes_jetEta1v3_2022-02-18.root
+  // jetEventPlaneDeltaPhi_PbPbMC2018_caloJets_multWeight_allEventPlanes_jetEta1v3_2022-02-17.root
+  // jetEventPlaneDeltaPhi_PbPbMC2018_caloJets_multWeight_forestEventPlanes_jetEta1v3_2022-02-18.root
+  // jetEventPlaneDeltaPhi_PbPb2018_pfCsJets_forestEventPlanes_jetEta1v6_2022-02-18.root
+  // jetEventPlaneDeltaPhi_PbPbMC2018_pfCsJets_multWeight_allEventPlanes_jetEta1v6_2022-02-17.root
+  // jetEventPlaneDeltaPhi_PbPbMC2018_pfCsJets_multWeight_forestEventPlanes_jetEta1v6_2022-02-18.root
   
-  TString jetTypeString[4] = {"Calo jet","PFCS jet","Calo tuned Q","Gen jet"};
+  TString jetTypeString[4] = {"MC, calo jet","MC, PFCS jet","Calo tuned Q","Gen jet"};
   //TString jetTypeString[4] = {"Whole #eta","#eta strip","reflected #eta strip","Gen jet"};
   
   int eventPlaneOrder = 2; // The vn component that is plotted
@@ -23,13 +29,15 @@ void fitJetEventPlaneVnMultiplicity(){
   
   bool drawEventPlaneDifference = false;  // Draw difference between manual event plane calculation to the one read from forest
   
+  bool drawFits = false; // Draw the distributions and fits
   bool drawAllInSamePlot = false;  // True: Draw all three jet collection to the same plot. False: Use separate plots
   bool hideFit = false;
   
   bool printVs = false; // True = Print the jet vn values to the console. False = Do not do that
   
-  bool saveFigures = false;
-  TString saveComment = "_eventPlane4";
+  bool saveFigures = true;
+  TString saveComment = "_mcComparison";
+  TString figureFormat = "png";
   
   // Read the histograms from the data files
   const int nMultiplicityBins = 20;
@@ -44,8 +52,10 @@ void fitJetEventPlaneVnMultiplicity(){
   
   double xValues[16] = {500,700,900,1100,1300,1500,1700,1900,2100,2300,2500,2700,2900,3100,3300,3500};
   double xErrors[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  double yValues[16];
-  double yErrors[16];
+  double yValues[nFiles][16];
+  double yErrors[nFiles][16];
+  double yValuesRatio[nFiles-1][16];
+  double yErrorsRatio[nFiles-1][16];
   
   for(int iJetType = 0; iJetType < nFiles; iJetType++){
     for(int iMultiplicity = 0; iMultiplicity < nMultiplicityBins; iMultiplicity++){  // nQvectorBins
@@ -131,26 +141,82 @@ void fitJetEventPlaneVnMultiplicity(){
     
   
   // Collect the values for the y-axis arrays to illustrate the multiplicity dependence
-  for(int iMultiplicity = firstPlottedBin; iMultiplicity <= lastPlottedBin; iMultiplicity++){
-    yValues[iMultiplicity-firstPlottedBin] = fitFunctionMidRapidity[0][iMultiplicity]->GetParameter(eventPlaneOrder);
-    yErrors[iMultiplicity-firstPlottedBin] = fitFunctionMidRapidity[0][iMultiplicity]->GetParError(eventPlaneOrder);
+  double errorTerm1, errorTerm2;
+  for(int iFile = 0; iFile < nFiles; iFile++){
+    for(int iMultiplicity = firstPlottedBin; iMultiplicity <= lastPlottedBin; iMultiplicity++){
+      yValues[iFile][iMultiplicity-firstPlottedBin] = fitFunctionMidRapidity[iFile][iMultiplicity]->GetParameter(eventPlaneOrder);
+      yErrors[iFile][iMultiplicity-firstPlottedBin] = fitFunctionMidRapidity[iFile][iMultiplicity]->GetParError(eventPlaneOrder);
+      
+      if(iFile > 0){
+        yValuesRatio[iFile-1][iMultiplicity-firstPlottedBin] = yValues[iFile][iMultiplicity-firstPlottedBin] / yValues[0][iMultiplicity-firstPlottedBin];
+        errorTerm1 = yErrors[iFile][iMultiplicity-firstPlottedBin] / yValues[0][iMultiplicity-firstPlottedBin];
+        errorTerm2 = yErrors[0][iMultiplicity-firstPlottedBin] * yValues[iFile][iMultiplicity-firstPlottedBin] / (yValues[0][iMultiplicity-firstPlottedBin] * yValues[0][iMultiplicity-firstPlottedBin]);
+        yErrorsRatio[iFile-1][iMultiplicity-firstPlottedBin] = TMath::Sqrt(errorTerm1*errorTerm1 + errorTerm2*errorTerm2);
+      }
+      
+    }
   }
   
+  int markerColors[] = {kBlue, kRed, kGreen+3, kMagenta};
+  int markerStyles[] = {kFullSquare, kFullCircle, kFullDiamond, kFullCross};
   
   // Draw a graph illustrating the multiplicity dependence of the jet v2
-  TGraphErrors *multiplicityGraph = new TGraphErrors(lastPlottedBin-firstPlottedBin+1, xValues, yValues, xErrors, yErrors);
-  multiplicityGraph->SetMarkerStyle(kFullSquare);
-  multiplicityGraph->SetMarkerColor(kBlue);
+  TGraphErrors *multiplicityGraph[nFiles];
+  TGraphErrors *multiplicityRatio[nFiles-1];
+  for(int iFile = 0; iFile < nFiles; iFile++){
+    multiplicityGraph[iFile] = new TGraphErrors(lastPlottedBin-firstPlottedBin+1, xValues, yValues[iFile], xErrors, yErrors[iFile]);
+    multiplicityGraph[iFile]->SetMarkerStyle(markerStyles[iFile]);
+    multiplicityGraph[iFile]->SetMarkerColor(markerColors[iFile]);
+    
+    if(iFile > 0){
+      multiplicityRatio[iFile-1] = new TGraphErrors(lastPlottedBin-firstPlottedBin+1, xValues, yValuesRatio[iFile-1], xErrors, yErrorsRatio[iFile-1]);
+      multiplicityRatio[iFile-1]->SetMarkerStyle(markerStyles[iFile]);
+      multiplicityRatio[iFile-1]->SetMarkerColor(markerColors[iFile]);
+    }
+  }
   
+  TLegend *legend;
   JDrawer *drawer = new JDrawer();
   drawer->SetDefaultAppearanceGraph();
   
-  double highYrange[] = {0.2,0.2,0.14,0.06,0.03,0.02,0.02};
+  double highYrange[] = {0.2,0.2,0.25,0.06,0.03,0.02,0.02};
   
-  drawer->DrawGraph(multiplicityGraph, 400, 3600, 0.0, highYrange[eventPlaneOrder], "Multiplicity", Form("Jet v_{%d}", eventPlaneOrder), " ", "p");
+  // Draw the multiplicity graphs
+  drawer->DrawGraph(multiplicityGraph[0], 400, 3600, 0.0, highYrange[eventPlaneOrder], "Multiplicity", Form("Jet v_{%d} bias", eventPlaneOrder), " ", "p");
+  for(int iFile = 1; iFile < nFiles; iFile++){
+    multiplicityGraph[iFile]->Draw("same,p");
+  }
+  
+  // Add a legend to the plot
+  legend = new TLegend(0.3,0.2,0.5,0.4);
+  legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
+  for(int iFile = 0; iFile < nFiles; iFile++){
+    legend->AddEntry(multiplicityGraph[iFile], jetTypeString[iFile], "p");
+  }
+  legend->Draw();
   
   if(saveFigures){
-    gPad->GetCanvas()->SaveAs(Form("figures/jetV%dvsMultiplicity%s.pdf", eventPlaneOrder, saveComment.Data()));
+    gPad->GetCanvas()->SaveAs(Form("figures/jetV%dvsMultiplicity%s.%s", eventPlaneOrder, saveComment.Data(), figureFormat.Data()));
+  }
+  
+  // If there is a ratio file, draw it
+  if(nFiles > 1){
+    drawer->DrawGraph(multiplicityRatio[0], 400, 3600, 0.0, 3.5, "Multiplicity", Form("Jet v_{%d} ratio", eventPlaneOrder), " ", "p");
+    for(int iFile = 2; iFile < nFiles; iFile++){
+      multiplicityRatio[iFile-1]->Draw("same,p");
+    }
+  }
+  
+  // Add a legend to the ratio plot
+  legend = new TLegend(0.23,0.2,0.43,0.4);
+  legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
+  for(int iFile = 1; iFile < nFiles; iFile++){
+    legend->AddEntry(multiplicityRatio[iFile-1], Form("%s / %s", jetTypeString[iFile].Data(), jetTypeString[0].Data()), "p");
+  }
+  legend->Draw();
+  
+  if(saveFigures){
+    gPad->GetCanvas()->SaveAs(Form("figures/jetV%dvsMultiplicityRatio%s.%s", eventPlaneOrder, saveComment.Data(), figureFormat.Data()));
   }
   
   // Draw the jet-event plane fits
@@ -158,91 +224,92 @@ void fitJetEventPlaneVnMultiplicity(){
 
   TString multiplicityString;
   TString compactMultiplicityString;
-  TLegend *legend;
   int colors[] = {kBlack, kRed, kBlue, kGreen+3};
   double maxYscale, minYscale;
 
-  for(int iMultiplicity = firstPlottedBin; iMultiplicity <= lastPlottedBin; iMultiplicity++){
-
-    multiplicityString = Form("Multiplicity: %.0f-%.0f%%", multiplicityBinBorders[iMultiplicity], multiplicityBinBorders[iMultiplicity+1]);
-    compactMultiplicityString = Form("_M=%.0f-%.0f", multiplicityBinBorders[iMultiplicity], multiplicityBinBorders[iMultiplicity+1]);
-
-    if(drawAllInSamePlot){
-      legend = new TLegend(0.2,0.68,0.4,1);
-      legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
-      legend->AddEntry((TObject*)0, Form("%s, manual jet energy correction",multiplicityString.Data()), "");
-    }
-
-    maxYscale = jetEventPlaneMidRapidity[referenceYield][iMultiplicity]->GetMaximum();
-    maxYscale = maxYscale + 0.2*maxYscale; // 0.1
-    minYscale = jetEventPlaneMidRapidity[referenceYield][iMultiplicity]->GetMinimum();
-    minYscale = minYscale - 0.02*minYscale;
-
-    for(int iJetType = 0; iJetType < nFiles; iJetType++){
-
-      if(hideFit) jetEventPlaneMidRapidity[iJetType][iMultiplicity]->RecursiveRemove(fitFunctionMidRapidity[iJetType][iMultiplicity]);
-
-      jetEventPlaneMidRapidity[iJetType][iMultiplicity]->Rebin(2);
-      jetEventPlaneMidRapidity[iJetType][iMultiplicity]->Scale(0.5);
-      jetEventPlaneMidRapidity[iJetType][iMultiplicity]->GetYaxis()->SetRangeUser(minYscale, maxYscale);
-      if(drawAllInSamePlot) {
-        jetEventPlaneMidRapidity[iJetType][iMultiplicity]->SetLineColor(colors[iJetType]);
-        fitFunctionMidRapidity[iJetType][iMultiplicity]->SetLineColor(colors[iJetType]);
-
-      }
-
-      if(!drawAllInSamePlot || iJetType == 0){
-      drawer->DrawHistogram(jetEventPlaneMidRapidity[iJetType][iMultiplicity], "#Delta#varphi", "A.U.", " ");
-      } else {
-        jetEventPlaneMidRapidity[iJetType][iMultiplicity]->Draw("same");
-      }
-
-      if(!drawAllInSamePlot){
-        legend = new TLegend(0.2,0.7,0.4,0.9);
+  if(drawFits){
+    for(int iMultiplicity = firstPlottedBin; iMultiplicity <= lastPlottedBin; iMultiplicity++){
+      
+      multiplicityString = Form("Multiplicity: %.0f-%.0f%%", multiplicityBinBorders[iMultiplicity], multiplicityBinBorders[iMultiplicity+1]);
+      compactMultiplicityString = Form("_M=%.0f-%.0f", multiplicityBinBorders[iMultiplicity], multiplicityBinBorders[iMultiplicity+1]);
+      
+      if(drawAllInSamePlot){
+        legend = new TLegend(0.2,0.68,0.4,1);
         legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
-        legend->AddEntry((TObject*)0, Form("%s, #Psi_{%d}",multiplicityString.Data(), eventPlaneOrder), "");
-        legend->AddEntry((TObject*)0, jetTypeString[iJetType], "");
-        legend->Draw();
-
-        if(saveFigures){
-          gPad->GetCanvas()->SaveAs(Form("figures/jetEventPlaneDeltaPhi%s%s.pdf", saveComment.Data(), compactMultiplicityString.Data()));
+        legend->AddEntry((TObject*)0, Form("%s, manual jet energy correction",multiplicityString.Data()), "");
+      }
+      
+      maxYscale = jetEventPlaneMidRapidity[referenceYield][iMultiplicity]->GetMaximum();
+      maxYscale = maxYscale + 0.2*maxYscale; // 0.1
+      minYscale = jetEventPlaneMidRapidity[referenceYield][iMultiplicity]->GetMinimum();
+      minYscale = minYscale - 0.02*minYscale;
+      
+      for(int iJetType = 0; iJetType < nFiles; iJetType++){
+        
+        if(hideFit) jetEventPlaneMidRapidity[iJetType][iMultiplicity]->RecursiveRemove(fitFunctionMidRapidity[iJetType][iMultiplicity]);
+        
+        jetEventPlaneMidRapidity[iJetType][iMultiplicity]->Rebin(2);
+        jetEventPlaneMidRapidity[iJetType][iMultiplicity]->Scale(0.5);
+        jetEventPlaneMidRapidity[iJetType][iMultiplicity]->GetYaxis()->SetRangeUser(minYscale, maxYscale);
+        if(drawAllInSamePlot) {
+          jetEventPlaneMidRapidity[iJetType][iMultiplicity]->SetLineColor(colors[iJetType]);
+          fitFunctionMidRapidity[iJetType][iMultiplicity]->SetLineColor(colors[iJetType]);
+          
         }
-
-      } else {
-        legend->AddEntry(jetEventPlaneMidRapidity[iJetType][iMultiplicity], Form("%s, v_{2} = %.3f", jetTypeString[iJetType].Data(), fitFunctionMidRapidity[iJetType][iMultiplicity]->GetParameter(2)) ,"l");
+        
+        if(!drawAllInSamePlot || iJetType == 0){
+          drawer->DrawHistogram(jetEventPlaneMidRapidity[iJetType][iMultiplicity], "#Delta#varphi", "A.U.", " ");
+        } else {
+          jetEventPlaneMidRapidity[iJetType][iMultiplicity]->Draw("same");
+        }
+        
+        if(!drawAllInSamePlot){
+          legend = new TLegend(0.2,0.7,0.4,0.9);
+          legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
+          legend->AddEntry((TObject*)0, Form("%s, #Psi_{%d}",multiplicityString.Data(), eventPlaneOrder), "");
+          legend->AddEntry((TObject*)0, jetTypeString[iJetType], "");
+          legend->Draw();
+          
+          if(saveFigures){
+            gPad->GetCanvas()->SaveAs(Form("figures/jetEventPlaneDeltaPhi%s%s.%s", saveComment.Data(), compactMultiplicityString.Data(), figureFormat.Data()));
+          }
+          
+        } else {
+          legend->AddEntry(jetEventPlaneMidRapidity[iJetType][iMultiplicity], Form("%s, v_{2} = %.3f", jetTypeString[iJetType].Data(), fitFunctionMidRapidity[iJetType][iMultiplicity]->GetParameter(2)) ,"l");
+        }
+        
+      } // Jet type loop
+      if(drawAllInSamePlot){
+        legend->Draw();
+        
+        if(saveFigures){
+          gPad->GetCanvas()->SaveAs(Form("figures/jetEventPlaneDeltaPhi%s_jetTypeComparison%s.%s", saveComment.Data(), compactMultiplicityString.Data(), figureFormat.Data()));
+        }
       }
-
-    } // Jet type loop
-    if(drawAllInSamePlot){
-      legend->Draw();
-
-      if(saveFigures){
-        gPad->GetCanvas()->SaveAs(Form("figures/jetEventPlaneDeltaPhi%s_jetTypeComparison%s.pdf", saveComment.Data(), compactMultiplicityString.Data()));
+      
+      if(drawEventPlaneDifference){
+        
+        legend = new TLegend(0.5,0.7,0.8,0.85);
+        legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
+        legend->AddEntry((TObject*)0, multiplicityString, "");
+        
+        jetEventPlaneDifference[iMultiplicity]->Fit("gaus","","",-0.5,0.5);
+        
+        legend->AddEntry((TObject*)0, Form("#sigma = %.3f", jetEventPlaneDifference[iMultiplicity]->GetFunction("gaus")->GetParameter(2)), "");
+        
+        jetEventPlaneDifference[iMultiplicity]->SetLineColor(kBlack);
+        jetEventPlaneDifference[iMultiplicity]->Rebin(2);
+        jetEventPlaneDifference[iMultiplicity]->Scale(0.5);
+        
+        drawer->DrawHistogram(jetEventPlaneDifference[iMultiplicity], "#Delta#varphi", "A.U.", "Difference between manual and HiForest event plane");
+        
+        legend->Draw();
+        
+        if(saveFigures){
+          gPad->GetCanvas()->SaveAs(Form("figures/jetEventPlaneDeltaPhiDifference%s%s.%s", saveComment.Data(), compactMultiplicityString.Data(), figureFormat.Data()));
+        }
+        
       }
-    }
-
-    if(drawEventPlaneDifference){
-
-      legend = new TLegend(0.5,0.7,0.8,0.85);
-      legend->SetFillStyle(0);legend->SetBorderSize(0);legend->SetTextSize(0.05);legend->SetTextFont(62);
-      legend->AddEntry((TObject*)0, multiplicityString, "");
-
-      jetEventPlaneDifference[iMultiplicity]->Fit("gaus","","",-0.5,0.5);
-
-      legend->AddEntry((TObject*)0, Form("#sigma = %.3f", jetEventPlaneDifference[iMultiplicity]->GetFunction("gaus")->GetParameter(2)), "");
-
-      jetEventPlaneDifference[iMultiplicity]->SetLineColor(kBlack);
-      jetEventPlaneDifference[iMultiplicity]->Rebin(2);
-      jetEventPlaneDifference[iMultiplicity]->Scale(0.5);
-
-      drawer->DrawHistogram(jetEventPlaneDifference[iMultiplicity], "#Delta#varphi", "A.U.", "Difference between manual and HiForest event plane");
-
-      legend->Draw();
-
-      if(saveFigures){
-        gPad->GetCanvas()->SaveAs(Form("figures/jetEventPlaneDeltaPhiDifference%s%s.pdf", saveComment.Data(), compactMultiplicityString.Data()));
-      }
-
-    }
-  } // Multiplicity loop
+    } // Multiplicity loop
+  } // Drawing fits
 }
